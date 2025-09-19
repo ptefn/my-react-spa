9279d11b48b50f8e31eff0c2f3758b33656529c7
ptefn <i.ptefn@gmail.com>
2025-09-19 13:42:50 +0700

    Добавил компонент шапки сайта и навигацию

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // App.js
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import Home from './pages/Home';
 import About from './pages/About';
 
